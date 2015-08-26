12338df24b611bb6c1d0daa0552e79217a3ed66c
Jake V <zeronamagem@gmail.com>
2015-08-26 13:06:39 -0500

    Program 2 - In main, created displayBrokenLinks function Added a if statement to stop it from crawling to the external Duke website, can be commented out if we want to. Formatted print statements Added comments, still need more

--- src/vosters/program3/WebCrawler.java
+++ src/vosters/program3/WebCrawler.java
@@ -17,13 +17,58 @@ public class WebCrawler {
 		//Setup Arraylists
 		ArrayList<URL> pagesVisited = new ArrayList<URL>();
 		ArrayList<URL> pagesToVisit = new ArrayList<URL>();
-
 		ArrayList<ArrayList<String>> brokenLinks = new ArrayList<ArrayList<String>>();
+
+		//get starting URL from user
 		URL startingURL = getStartingURLFromUser();
-		String test = htmlReader(startingURL);
-		linkParser(test);
-		System.out.println(test);
-		System.out.println(isBrokenLink(startingURL, "http://www.cs.uwec.edu/~stevende/cs145testpages/page1.htm"));
+
+		pagesToVisit.add(startingURL);
+
+
+		while(!pagesToVisit.isEmpty()){ //loop through all the pages
+			ArrayList<String> links = linkParser(htmlReader(pagesToVisit.get(0))); //parse the links from the current page's html contents
+
+
+			ArrayList<String> curLink = new ArrayList<String>();
+			brokenLinks.add(curLink);
+			while(!links.isEmpty()){ //loop through all the links to determine if they are broken
+
+				//if the link is broken
+				if(isBrokenLink(pagesToVisit.get(0), links.get(0))){
+					if(curLink.isEmpty()){
+						//Set the page the broken link is on to element 1 of the broken links array
+						curLink.add(pagesToVisit.get(0).toString());
+					}
+					System.out.println("On page: " + pagesToVisit.get(0).toString() + " ---- broken link: " + links.get(0));
+					curLink.add(links.get(0));
+					links.remove(0);
+				} else {
+
+					//if the link is NOT broken
+					try {
+						//We don't want to actually go to external websites. So skip going to the goduke link for now
+						if(links.get(0).indexOf("www.goduke") < 0){
+							//Adds link to pages to visit (if it's not already on our list and we already haven't visited it)
+							if(!pagesToVisit.get(0).equals(new URL(pagesToVisit.get(0),links.get(0))) && !pagesVisited.contains(new URL(pagesToVisit.get(0),links.get(0)))){
+								pagesToVisit.add(new URL(pagesToVisit.get(0),links.get(0)));
+							}
+						}
+					} catch (MalformedURLException e) {
+					}
+					links.remove(0);
+				}
+			}
+
+			//if(pagesToVisit.get(0).toString().indexOf("http://www.cs") != -1){ //only test these internal sites
+			pagesVisited.add(pagesToVisit.get(0)); //mark the page as already visited
+			pagesToVisit.remove(0);
+			//}
+
+		}
+
+
+		displayBrokenLinkReport(brokenLinks);
+
 	}
 
 
@@ -35,7 +80,7 @@ public class WebCrawler {
 			// Get the HTML from the user
 			//*********************************************************************	
 			//URL startingURL = new URL(JOptionPane.showInputDialog("Please enter a URL: "));
-			startingURL = new URL("http://www.cs.uwec.edu/~stevende/cs145testpages/page1.htm"); //Hard coded	
+			startingURL = new URL("http://www.cs.uwec.edu/~stevende/cs145testpages/default.htm"); //Hard coded	
 
 		} catch (MalformedURLException e) {
 			System.out.println("Bad URL");
@@ -64,7 +109,7 @@ public class WebCrawler {
 			br.close();			
 
 		} catch (IOException e) {
-			System.out.println("Error reading HTML");
+			System.out.println("Error reading HTML " + webURL + "\nSkipped file");
 			System.exit(1);
 		}
 		//********************************
@@ -81,47 +126,65 @@ public class WebCrawler {
 		ArrayList<String> urls = new ArrayList<String>();
 		int index = 0;
 		while(htmlContents.indexOf("<a href=",index) != -1){ //while there is still another anchor left in the string
-			urls.add(htmlContents.substring(htmlContents.indexOf("<a href=", index) + 9,htmlContents.indexOf("\">", htmlContents.indexOf("<a href=", index))));
-			index = htmlContents.indexOf("\">", htmlContents.indexOf("<a href=", index));
+			urls.add(htmlContents.substring(htmlContents.indexOf("<a href=", index) + 9,htmlContents.indexOf("\"", htmlContents.indexOf("<a href=", index) + 9)));
+			index = htmlContents.indexOf("\"", htmlContents.indexOf("<a href=", index) + 9);
 		}
 		return urls;
-		
+
 	}
 
 	public static boolean isBrokenLink(URL baseURL, String theHREF){
 		URL newLink;
-		
+
 		//*************************************
 		//Check to see if the link is malformed
 		try {
-			newLink = new URL(theHREF);
+			newLink = new URL(baseURL,theHREF);
 		} catch (MalformedURLException e) {
 			return true;
 		}
-		
+
 		//*************************************
 		//Check to see if the server exists		
 		URLConnection con;
 		try {
 			con = newLink.openConnection();
+			con.connect();
 		} catch (IOException e) {
 			return true;
 		}
-		
+
 		//*************************************
 		//Check to see if there is a 404 error
 		HttpURLConnection httpProtocol = (HttpURLConnection)con;
 		try {
+			System.out.println(httpProtocol.getResponseCode()); //print page response code
 			if(httpProtocol.getResponseCode() == 404){
 				return true;
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		return false; //return false if this is a working link!
 	}
 
 	public static void displayBrokenLinkReport(ArrayList<ArrayList<String>>brokenLinks){
+
+		System.out.println("\nBroken Link Report: ");
+		for(int i = 0; i < brokenLinks.size(); i++){
+			for(int j = 0; j < brokenLinks.get(i).size();j++){
+				if(j == 0){
+					System.out.println("    Page: " + brokenLinks.get(i).get(j));
+				} else {
+					System.out.println("         Broken link: " + brokenLinks.get(i).get(j));
+				}
+			}
+		}
+
+
+
+
+
 	}
 }
